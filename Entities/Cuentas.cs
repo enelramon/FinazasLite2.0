2a086abaf61cc43a0f1e8121af94b61ea674f34e
Allg17 <albert_lopez@ucne.edu.do>
2018-09-05 21:06:55 -0400

    Agregando La Entidad Cuentas

---
+++ Entities/Cuentas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Cuentas
+    {
+        [Key]
+        public int CuentaId { get; set; }
+        public string Descripcion { get; set; }
+        public Origenes Origen { get; set; }
+
+        public Cuentas(int cuentaId, string descripcion, Origenes origen)
+        {
+            CuentaId = cuentaId;
+            Descripcion = descripcion;
+            Origen = origen;
+        }
+
+        public Cuentas()
+        {
+            CuentaId = 0;
+            Descripcion = string.Empty;
+            Origen = 0;
+        }
+    }
+}
