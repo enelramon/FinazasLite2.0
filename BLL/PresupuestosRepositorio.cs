1eb50338121ec850ea27019a8cf9f6b6c16523ca
Allg17 <albert_lopez@ucne.edu.do>
2018-09-12 20:46:02 -0400

    Agregando Test y logica de acumulacion de tiposEgresos primera fase

--- BLL/PresupuestosRepositorio.cs
+++ BLL/PresupuestosRepositorio.cs
@@ -1,4 +1,5 @@
-﻿using Entities;
+﻿using DAL;
+using Entities;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -8,8 +9,40 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    public class PresupuestosRepositorio: RepositorioBase<Presupuestos>
+    public class PresupuestosRepositorio : RepositorioBase<Presupuestos>
     {
+        public override bool Guardar(Presupuestos entity)
+        {
+
+            bool paso = false;
+            _contexto = new DAL.Contexto();
+            try
+            {
+                if (AcumularEgreso(entity.Detalle) && base.Guardar(entity))
+                    paso = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        private bool AcumularEgreso(List<PresupuestosDetalle> Detalle)
+        {
+            bool paso = false;
+            BLL.RepositorioBase<TiposEgresos> contexto = new BLL.RepositorioBase<TiposEgresos>();
+            foreach (var item in Detalle)
+            {
+                var egreso = contexto.Buscar(item.TipoEgresoId);
+                egreso.Acumulado += item.Monto;
+                contexto.Modificar(egreso);
+                paso = true;
+            }
+
+            return paso;
+
+        }
 
         public override Presupuestos Buscar(int id)
         {
@@ -43,7 +76,7 @@ namespace BLL
                 foreach (var item in presupuesto.Detalle)
                 {
                     //Muy importante indicar que pasara con la entidad del detalle
-                    var estado = item.Id > 0 ? EntityState.Modified : EntityState.Added; 
+                    var estado = item.Id > 0 ? EntityState.Modified : EntityState.Added;
                     _contexto.Entry(item).State = estado;
                 }
 
@@ -57,6 +90,45 @@ namespace BLL
             {
                 throw;
             }
+            return paso;
+        }
+
+        public override bool Eliminar(int id)
+        {
+            bool paso = false;
+            _contexto = new Contexto();
+            var presupuesto = _contexto.Presupuestos.Find(id);
+            presupuesto.Detalle.Count();
+            try
+            {
+               
+                foreach (var item in presupuesto.Detalle)
+                {
+                    item.TipoEgreso = null;
+                }
+                
+                if(EliminarAcumulado(presupuesto.Detalle) && base.Eliminar(id))
+                    paso = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return paso;
+        }
+
+        private bool EliminarAcumulado(List<PresupuestosDetalle> Detalle)
+        {
+            bool paso = false;
+            BLL.RepositorioBase<TiposEgresos> contexto = new BLL.RepositorioBase<TiposEgresos>();
+            foreach (var item in Detalle)
+            {
+                var egreso = contexto.Buscar(item.TipoEgresoId);
+                egreso.Acumulado -= item.Monto;
+                contexto.Modificar(egreso);
+                paso = true;
+            }
+
             return paso;
         }
 
--- FinanzasLite2.0.Tests/PresupuestosRepositorioTests.cs
+++ FinanzasLite2.0.Tests/PresupuestosRepositorioTests.cs
@@ -5,22 +5,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities;
 
 namespace BLL.Tests
 {
     [TestClass()]
     public class PresupuestosRepositorioTests
     {
-        [TestMethod()]
-        public void BuscarTest()
+       [TestMethod]
+        public void GuardarPresupuesto()
         {
-            Assert.Fail();
+            BLL.PresupuestosRepositorio repositorio = new BLL.PresupuestosRepositorio();
+            var paso = repositorio.Guardar(GetPresupuesto());
+            Assert.IsTrue(paso);
         }
 
-        [TestMethod()]
-        public void ModificarTest()
+        private Presupuestos GetPresupuesto()
         {
-            Assert.Fail();
+            Presupuestos presupuestos = new Presupuestos();
+            presupuestos.Descripcion = "Prueba";
+            presupuestos.PresupuestoId = 0;
+            presupuestos.AgregarDetalle(0, 0, 4, 500);
+            return presupuestos;
         }
+
+        [TestMethod]
+        public void GuardarEgreso()
+        {
+            BLL.RepositorioBase<TiposEgresos> repositorio = new BLL.RepositorioBase<TiposEgresos>();
+            var paso = repositorio.Guardar(GetTiposEgresos());
+            Assert.IsTrue(paso);
+        }
+
+        private TiposEgresos GetTiposEgresos()
+        {
+            TiposEgresos egreso = new TiposEgresos();
+            egreso.Acumulado = 0;
+            egreso.Descripcion = "Prueba";
+            egreso.EsActivo = true;
+            egreso.TipoEgresoId = 0;
+            return egreso;
+
+        }
+
+        [TestMethod]
+        public void EliminarPresupuesto()
+        {
+            BLL.RepositorioBase<TiposEgresos> repositorio = new BLL.RepositorioBase<TiposEgresos>();
+            var paso = repositorio.Eliminar(4);
+            Assert.IsTrue(paso);
+        }
+
     }
 }
